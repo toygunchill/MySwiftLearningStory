a2b6814983c39d4d8329ababc9d3217eb805a306
Toygun Çil <toyguncil1@gmail.com>
2022-09-04 20:00:17 +0300

    created a newproject

---
+++ SeyahatDefteri/SeyahatDefteri/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeyahatDefteri
+//
+//  Created by Toygun Çil on 4.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
